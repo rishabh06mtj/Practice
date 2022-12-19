a231f69ebd553246329e483c4c6d563e3e47f4f0
rishabh06mtj <104261562+rishabh06mtj@users.noreply.github.com>
2022-12-19 17:31:44 +0530

    Update FlightsPage.java

--- src/main/java/com/spicejet/objectRepository/FlightsPage.java
+++ src/main/java/com/spicejet/objectRepository/FlightsPage.java
@@ -2,7 +2,8 @@ package com.spicejet.objectRepository;
 import java.util.List;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-//
+//coomit remote master branch
+
 public class FlightsPage {
 @FindBy(xpath="//div[text()='Select your' and text()='Departure to']/parent::div/parent::div/parent::div/following-sibling::div/descendant::div[text()='SG 535']/parent::div/parent::div/parent::div/following-sibling::div//span/parent::div")
 private List<WebElement> flightDeparturePrice;
